febbbd5f336c68781e47f3354994d0c8285add63
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-09 16:23:07 +0700

    Divide into modules

--- Hangman.py
+++ Hangman.py
@@ -1,25 +1,8 @@
 import random
-
-animals = ["rabbit", "tiger", "elephant"]
-countries = ["usa", "ukraine", "russia"]
-painters = ["rembrandt", "monet", "dali"]
-topics = [animals, countries, painters]
-
+from hangman_arts import stages
+from hangman_topics import topics
 def print_hung(hung):
-    if hung == 0:
-        print("\n" * 8)
-    elif hung == 1:
-        print(" |----\n |\n |\n |\n |\n |\n |\n |\n/ \\")
-    elif hung == 2:
-        print(" |----\n |   |\n |\n |\n |\n |\n |\n |\n/ \\")
-    elif hung == 3:
-        print(" |----\n |   |\n |   O\n |\n |\n |\n |\n |\n/ \\")
-    elif hung == 4:
-        print(" |----\n |   |\n |   O\n |   |\n |   |\n |\n |\n |\n/ \\")
-    elif hung == 5:
-        print(" |----\n |   |\n |   O\n |  /|\ \n |   |\n |\n |\n |\n/ \\")
-    elif hung == 6:
-        print(" |----\n |   |\n |   O\n |  /|\ \n |   |\n |  / \ \n |\n |\n/ \\ GAME OVER")
+    print(stages[hung])
 
 def changeWord(word, symbol, index):
     word = word[:index] + symbol + word[index+1:]
@@ -34,7 +17,7 @@ def relace_user_word(st, sym, user_word):
     return user_word
 
 def create_hung_word():
-    choice = int(input("Choose a topic: 1.Animals\n2.Countries\n3.Painters"))
+    choice = int(input("Choose a topic: 1.Animals\n2.Countries\n3.Painters\n"))
     random_choice = random.randint(0, 2)
     hung_word = topics[choice-1][random_choice]
     return hung_word
@@ -66,6 +49,7 @@ def logic():
             exit()
         print_hung(hung)
         print(user_word)
+    print("YOU LOSE!")
 
 hung_word = create_hung_word()
 user_word = create_user_word()
---
+++ hangman_topics.py
@@ -0,0 +1,4 @@
+animals = ["rabbit", "tiger", "elephant", "dog", "puppy", "cat", "kitten", "parrot", "hamster", "chinchilla", "pig", "mouse", "rat", "chicken", "sheep", "turkey", "lamb", "goat", "donkey", "ram", "bear", "wolf", "deer", "raccoon"]
+countries = ["usa", "ukraine", "russia", "estonia", "finland", "ireland", "latvia", "lithuania", "norway", "scotland", "sweden", "wales", "austria", "france", "germany", "spain"]
+painters = ["rembrandt", "monet", "dali", "hogarth", "millais", "blake", "phillips", "dante", "spencer"]
+topics = [animals, countries, painters]
